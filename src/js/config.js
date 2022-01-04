5d723455596de431efce3d56eb752f93996f56f5
René Meier <74476633+meierrene@users.noreply.github.com>
2022-01-04 13:02:42 -0300

    Delete src directory

--- src/js/config.js
+++
@@ -1,7 +0,0 @@
-export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
-export const TIMEOUT_SEC = 10;
-export const RES_PER_PAGE = 10;
-export const KEY = 'cc3594af-0a72-4a49-b2bd-f9c3b711e6c9';
-export const MODAL_CLOSE_SEC = 1.5;
-
-// https://forkify-api.herokuapp.com/v2
