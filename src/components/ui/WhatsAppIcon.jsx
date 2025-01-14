bb28dc2b5f25002d9b4a36ff8420dac224214157
Rene Meier <renemeier.de@gmail.com>
2025-01-14 14:22:23 -0300

    readme.md adjustment

--- src/components/ui/WhatsAppIcon.jsx
+++ src/components/ui/WhatsAppIcon.jsx
@@ -3,7 +3,7 @@ import './WhatsAppIcon.css';
 const WhatsAppIcon = () => {
   return (
     <a
-      href="https://api.whatsapp.com/send?phone=5519984308466&text=Hey%21%20I%20found%20you%20in%20your%20portfolio."
+      href="Hi%20Ren%C3%A9%21%0AI%20found%20you%20in%20your%20portfolio%21"
       rel="noopener noreferrer"
       className="float"
       target="_blank"
